f29cb6dbb051444236486e739ff754d077496376
unknown <t.harish1502@gmail.com>
2026-03-20 14:37:47 -0400

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Harish-1502.github.io',
+  base: '/personal-website/src/pages/index.astro',
+})
