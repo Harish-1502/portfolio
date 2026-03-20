774730d0a5f83b7e50059d2e6bae336f817ec7aa
unknown <t.harish1502@gmail.com>
2026-03-20 14:58:24 -0400

    Fix GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://Harish-1502.github.io',
-  base: '/personal-website/src/pages/index.astro',
+  site: 'https://harish-1502.github.io',
+  base: '/portfolio',
 })
